bb65ff24b7e73974eafaf4ecbcec298cd4f96732
piyush-9211 <rt218878@gmail.com>
2026-03-18 11:09:09 +0530

    Coded UC4: Multiple names

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -4,7 +4,9 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello Guest");
         } else {
-            System.out.println("Hello " + args[0]);
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Hello " + args[i]);
+            }
         }
 
     }
