f423307b533e548c2ef850632205b8622fce9de6
piyush-9211 <rt218878@gmail.com>
2026-03-18 11:06:15 +0530

    Coded UC3

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,10 +1,10 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+        if (args.length == 0) {
+            System.out.println("Hello Guest");
         } else {
-            System.out.println("Hello World");
+            System.out.println("Hello " + args[0]);
         }
 
     }
