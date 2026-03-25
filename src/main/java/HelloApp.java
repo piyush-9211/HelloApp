1ddda673f8e2b229e3f0dcebb9ee70896ddcef46
piyush-9211 <rt218878@gmail.com>
2026-03-25 08:51:45 +0530

    Coded UC7 using String.join()

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,18 +1,15 @@
 public class HelloApp {
     public static void main(String[] args) {
 
+        String greeting;
+
         if (args.length == 0) {
-            System.out.println("Hello, World!");
+            greeting = "Hello, World!";
         } else {
-            StringBuilder nameBuilder = new StringBuilder();
-
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-
-            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
-
-            System.out.println("Hello, " + names + "!");
+            String names = String.join(", ", args);
+            greeting = "Hello, " + names + "!";
         }
+
+        System.out.println(greeting);
     }
 }
