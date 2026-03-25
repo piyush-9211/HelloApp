476b6e40cfb796da2261d9c3ea72f83f7903422a
piyush-9211 <rt218878@gmail.com>
2026-03-25 08:47:21 +0530

    Coded UC6: substring method for delimiter removal

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -3,18 +3,16 @@ public class HelloApp {
 
         if (args.length == 0) {
             System.out.println("Hello, World!");
-            return;
-        }
+        } else {
+            StringBuilder nameBuilder = new StringBuilder();
 
-        StringBuilder names = new StringBuilder();
-
-        for (String name : args) {
-            if (names.length() > 0) {
-                names.append(", ");
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
             }
-            names.append(name);
-        }
 
-        System.out.println("Hello, " + names + "!");
+            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+
+            System.out.println("Hello, " + names + "!");
+        }
     }
 }
