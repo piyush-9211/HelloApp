2efae8c80a4c4d69f458b21281deb64af7a6f8a0
piyush-9211 <rt218878@gmail.com>
2026-05-07 15:54:20 +0530

    UC7 - Computer makes random move

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,27 +1,96 @@
+import java.util.Random;
 
+/**
+ * TicTacToe
+ * UC7 allows the computer to make a random valid move.
+ */
 public class TicTacToe {
 
     static char[][] board = {
-            {'-', '-', '-'},
-            {'-', '-', '-'},
+            {'X', '-', '-'},
+            {'-', 'O', '-'},
             {'-', '-', '-'}
     };
 
-
+    /**
+     * Entry point of the program.
+     */
     public static void main(String[] args) {
 
-        placeMove(1, 1, 'X');
+        System.out.println("Board Before Computer Move:");
+        printBoard();
 
+        computerMove();
+
+        System.out.println("Board After Computer Move:");
         printBoard();
     }
 
+    /**
+     * Generates a random valid move for the computer.
+     */
+    static void computerMove() {
 
+        Random random = new Random();
+
+        while (true) {
+
+            int slot = random.nextInt(9) + 1;
+
+            int row = getRowFromSlot(slot);
+            int col = getColFromSlot(slot);
+
+            if (isValidMove(row, col)) {
+
+                placeMove(row, col, 'O');
+
+                System.out.println("Computer selected slot: " + slot);
+
+                break;
+            }
+        }
+    }
+
+    /**
+     * Converts slot number to row index.
+     */
+    static int getRowFromSlot(int slot) {
+
+        return (slot - 1) / 3;
+    }
+
+    /**
+     * Converts slot number to column index.
+     */
+    static int getColFromSlot(int slot) {
+
+        return (slot - 1) % 3;
+    }
+
+    /**
+     * Checks whether move is valid.
+     */
+    static boolean isValidMove(int row, int col) {
+
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+
+            return false;
+        }
+
+        return board[row][col] == '-';
+    }
+
+    /**
+     * Places symbol on board.
+     */
     static void placeMove(int row, int col, char symbol) {
 
         board[row][col] = symbol;
     }
 
-
+    /**
+     * Prints board.
+     */
     static void printBoard() {
 
         System.out.println("-------------");
